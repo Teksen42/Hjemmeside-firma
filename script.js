8eecbf5ccf67d6ad35f8fba9a498f10c73680db3
Mucco2 <161291343+Mucco2@users.noreply.github.com>
2025-08-09 17:59:18 +0200

    comit
    comit

--- script.js
+++ script.js
@@ -131,3 +131,12 @@ ddocument.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+    const menuToggle = document.getElementById('mobile-menu-toggle');
+    const mainNav = document.querySelector('.main-nav');
+
+    if (menuToggle && mainNav) {
+        menuToggle.addEventListener('click', function() {
+            mainNav.classList.toggle('is-open');
+        });
+    }
